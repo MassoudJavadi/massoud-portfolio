445d3ef0eb7c2a535d3622fa719065cf5b5b0cca
Masoud Javadi <77633327+MassoudJavadi@users.noreply.github.com>
2021-11-23 23:37:36 +0330

    update button

--- src/Pages/PortfoliosPage.js
+++ src/Pages/PortfoliosPage.js
@@ -9,7 +9,7 @@ const allButtons = ['All', ...new Set(portfolios.map(item => item.category))]
 
 function PortfoliosPage() {
     const [menuItem, setMenuItems] = useState(portfolios);
-    const [button, setButtons] = useState(allButtons);
+    const button, = allButtons;
 
     const filter = (button) => {
 
